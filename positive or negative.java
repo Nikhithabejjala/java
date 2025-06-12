b676cadda275a1b6594a7841ba9b7bed7e706682
Nikhithabejjala <nikhithabejjala@gmail.com>
2025-06-12 07:46:25 +0530

    Add files via upload

---
+++ positive or negative.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class positive or negative{
+    public static void main(String[] argrs){
+        Scanner sc=new Scanner (System.in);
+        int a=sc.nextInt();
+        if(a>0)
+        {
+            System.out.println("positive");
+        }
+        else {
+            System.out.println("negative");
+        }
+    }
+}
