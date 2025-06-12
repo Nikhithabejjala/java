950c41e5ac66a233ea9bc43bbe23faaf6e437492
Nikhithabejjala <nikhithabejjala@gmail.com>
2025-06-12 07:42:06 +0530

    Add files via upload

---
+++ cubic.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+import java.util.*;
+public class cubic{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=Math.pow(a,3);
+        System.out.println(b);
+        
+    }
+}
